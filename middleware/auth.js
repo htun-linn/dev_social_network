e3dfe301a12dd486c6645738abf47d2bda0cced8
htunlin <work.htunlin@gmail.com>
2025-08-14 20:27:18 +0630

    [ADD] profile

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,7 +1,8 @@
 const jwt = require('jsonwebtoken');
 const config = require('config');
+const User = require('../models/User')
 
-module.exports = function (req, res, next) {
+module.exports = async function (req, res, next) {
     //Get token from the header
     const token = req.header('x-auth-token');
 
@@ -14,8 +15,15 @@ module.exports = function (req, res, next) {
     try {
         const decoded = jwt.verify(token, config.get('jwtSecret'));
         req.user = decoded.user;
+
+        //Check if user still exist in db
+        const user = await User.findOne({ _id: decoded.user.id })
+        if (!user) {
+            return res.status(401).json({ msg: 'Token is not valid.' });
+        }
         next();
     } catch (error) {
+        console.error(error.messsage);
         res.status(401).json({ msg: 'Token is not valid.' });
     }
 }
